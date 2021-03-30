b9c83daf06f66175debe92aa94062d1a39dd6304
Nicole Kim <na_yeon_kim@brown.edu>
2021-03-29 20:25:24 -0700

    added

--- AppNavigator.js
+++ AppNavigator.js
@@ -3,9 +3,15 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Icon } from 'react-native-elements'
+import Icons from 'react-native-vector-icons/MaterialIcons';
 import LandingScreen from './components/Landing';
 import RegisterScreen from './components/Register';
 import LoginScreen from './components/Login';
+import FAQScreen from './components/FAQ';
+import MyOffersScreen from './components/MyOffers';
+import OfferScreen from './components/OfferPage';
+import AccountScreen from './components/Accounts';
+import AccountInfoScreen from './components/AccountInfo';
 
 const Stack = createStackNavigator()
 const Tab = createBottomTabNavigator()
@@ -24,12 +30,21 @@ function MainTabNavigator() {
       tabBarIcon: () => {
         let iconName;
         if (route.name == 'Offers') {
+          iconName = 'search-dollar'
+        } else if (route.name == 'FAQ') {
+          iconName = 'question-circle'
+        } else if (route.name == 'Accounts') {
+          iconName = 'chart-bar'
+        } else if (route.name == 'My Offers') {
           iconName = 'coins'
-        }
+        } 
         return <Icon type='font-awesome-5' name={iconName} size={24} color="white" />
       }
     })}>
       <Tab.Screen name='Offers' component={LoginScreen} />
+      <Tab.Screen name='Accounts' component={AccountScreen} />
+      <Tab.Screen name='My Offers' component={MyOffersScreen} />
+      <Tab.Screen name='FAQ' component={FAQScreen} />
     </Tab.Navigator>
   )
 }
@@ -64,6 +79,16 @@ function MainStackNavigator() {
         <Stack.Screen
           name='Login'
           component={MainTabNavigator}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='OfferPage'
+          component={OfferScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='AccountInfoPage'
+          component={AccountInfoScreen}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
